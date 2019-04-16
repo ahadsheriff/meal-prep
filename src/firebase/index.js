0157aaf0e13e4b7c7873328c918a0937fbcc8b7c
Ahad Sheriff <ahadsheriff@gmail.com>
2019-04-16 11:29:04 -0400

    firebase configuration

---
+++ src/firebase/index.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyBChIwmtRkEnDHORTQ4t68bUHyADFW_kuY",
+    authDomain: "gatsby-acdd2.firebaseapp.com",
+    databaseURL: "https://gatsby-acdd2.firebaseio.com",
+    projectId: "gatsby-acdd2",
+    storageBucket: "gatsby-acdd2.appspot.com",
+    messagingSenderId: "1025378498283"
+};
+firebase.initializeApp(config);
